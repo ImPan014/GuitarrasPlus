a32c7615d077dc28e7c9ad6057d0b928f42c637e
ImPan014 <luisfranciscojimenezmorales@gmail.com>
2023-03-21 18:01:34 -0600

    act

--- indexJS.js
+++ indexJS.js
@@ -13,9 +13,9 @@ fetch(url)
         data.forEach(guitarra => {
             //html += `<tr><td>${guitarra.nombre}</td><td>${guitarra.color}</td><td>${guitarra.precio}</td></tr>`;
             html += `
-            <div>
+            <div class ="guitarra">
+            <img src="${guitarra.imagen}" alt="" class="img-guitarra">
             <h3>${guitarra.nombre}</h3>
-            <img src="" alt="">
             <p>${guitarra.precio}</p>
             <p>${guitarra.color}</p>
             </div>`;
@@ -24,3 +24,34 @@ fetch(url)
         // Agregar el HTML generado
         guitarraDiv.innerHTML = html;
     });
+
+
+// Mostras guitarras electricas
+var miBoton = document.getElementById("Electricas");
+miBoton.addEventListener("click", function () {
+    // Seleccionar el elemento HTML
+    const guitarraDiv = document.getElementById('tabla-guitarras');
+    guitarraDiv.innerHTML = "";
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Recorrer los elementos del objeto
+            let html = '';
+            data.forEach(guitarra => {
+                if (guitarra.tipo === "electrica") {
+                    //html += `<tr><td>${guitarra.nombre}</td><td>${guitarra.color}</td><td>${guitarra.precio}</td></tr>`;
+                    html += `
+                    <div class ="guitarra">
+                        <img src="${guitarra.imagen}" alt="" class="img-guitarra">
+                        <h3>${guitarra.nombre}</h3>
+                        <p>${guitarra.precio}</p>
+                        <p>${guitarra.color}</p>
+                    </div>`;
+                }
+            });
+
+            // Agregar el HTML generado
+            guitarraDiv.innerHTML = html;
+        });
+});
